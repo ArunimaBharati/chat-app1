99aa12f3e9e26b8098b77c9416b0a99acb27ace7
ArunimaBharati <87299793+ArunimaBharati@users.noreply.github.com>
2021-12-01 13:31:01 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,17 @@
 
 //ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+    apiKey: "AIzaSyB2apMwhBkrWq-pN9Jd38WdnZWdR8DcnB0",
+    authDomain: "chat-app-95391.firebaseapp.com",
+    projectId: "chat-app-95391",
+    storageBucket: "chat-app-95391.appspot.com",
+    messagingSenderId: "950295476663",
+    appId: "1:950295476663:web:7221b5198fe2b978b5d032"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
     Room_names = childKey;
