936389082c5633918effec52efe7fedd31a539d4
ArunimaBharati <87299793+ArunimaBharati@users.noreply.github.com>
2021-12-06 13:47:15 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,22 +1,45 @@
 
 //ADD YOUR FIREBASE LINKS HERE
 const firebaseConfig = {
-    apiKey: "AIzaSyB2apMwhBkrWq-pN9Jd38WdnZWdR8DcnB0",
-    authDomain: "chat-app-95391.firebaseapp.com",
-    projectId: "chat-app-95391",
-    storageBucket: "chat-app-95391.appspot.com",
-    messagingSenderId: "950295476663",
-    appId: "1:950295476663:web:7221b5198fe2b978b5d032"
-  };
+  apiKey: "AIzaSyB2apMwhBkrWq-pN9Jd38WdnZWdR8DcnB0",
+  authDomain: "chat-app-95391.firebaseapp.com",
+  databaseURL: "https://chat-app-95391-default-rtdb.firebaseio.com",
+  projectId: "chat-app-95391",
+  storageBucket: "chat-app-95391.appspot.com",
+  messagingSenderId: "950295476663",
+  appId: "1:950295476663:web:7221b5198fe2b978b5d032"
+};
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  user_name=localStorage.getItem("user_name");
+  document.getElementById("user_name").innerHTML="Welcome "+user_name+"!";
+
+  function addRoom()
+    {
+      room_name=document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose:"adding roomname"
+      });
+      localStorage.setItem("room_name",room_name);
+      window.location="kwitter_page.html";
+    }
 
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
     Room_names = childKey;
    //Start code
 
+   console.log("Room Name-"+Room_names);
+      row="<div claas='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML+=row;
    //End code
    });});}
 getData();
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name",name);
+      window.location="kwitter_page.html";
+}
